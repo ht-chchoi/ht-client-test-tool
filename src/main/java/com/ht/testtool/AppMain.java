7c2afd2e9968a3ff0cd3daba728224ab3809dd21
ht-chchoi <chchoi@hdtel.co.kr>
2022-02-07 14:43:15 +0900

    logback 설정

--- src/main/java/com/ht/testtool/AppMain.java
+++ src/main/java/com/ht/testtool/AppMain.java
@@ -1,11 +1,15 @@
 package com.ht.testtool;
 
 import com.ht.testtool.ui.MainFrame;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AppMain {
   public static final String APP_VERSION = "0.1";
 
   public static void main(String[] args) {
+    log.info("===== App Start Ready =====");
     MainFrame.getInstance().setVisible(true);
+    log.info("===== App Start Complete =====");
   }
 }
